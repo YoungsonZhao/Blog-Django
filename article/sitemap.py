24a4610b1efc37670cfd94372369d9d735e26c6a
root <root@vultr.guest>
2017-11-08 01:38:22 +0000

    Update sitemap's change frequence from weekly to daily and add three cover images.

--- article/sitemap.py
+++ article/sitemap.py
@@ -14,8 +14,8 @@ from django.core.urlresolvers import reverse
 from article.models import Article
 
 class BlogSitemap(Sitemap):
-    changefreq = "weekly"
-    priority = 0.5
+    changefreq = "daily"
+    priority = 0.8
 
     def items(self):
         return Article.objects.all()
